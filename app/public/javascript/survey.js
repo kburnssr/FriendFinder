097c8697e282fbcf54887fb9964b5f891cd1b337
kburnssr <50921973+kburnssr@users.noreply.github.com>
2019-10-29 15:31:20 -0700

    Got all of the items implemented - final

--- app/public/javascript/survey.js
+++ app/public/javascript/survey.js
@@ -1,9 +1,12 @@
-$(document).on("ready", function(){
+
     $("#submit").on("click",function(event){
         console.log("click");
         event.preventDefault();
         var name = $("#name").val().trim();
         var photo = $("#photo").val().trim();
+        if(name=="" || photo==""){
+            return
+        }
         var q1 = parseInt($("select#q1").val());
         var q2 = parseInt($("select#q2").val());
         var q3 = parseInt($("select#q3").val());
@@ -26,13 +29,45 @@ $(document).on("ready", function(){
             url: "/api/friends"
         }).then(function(result){
             console.log(result);
-            var currentFriendScore = q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 + q10;
+            var currentFriendScore = 0;
+            for (j=0; j<friend.scores.length; j++){
+                if(isNaN(friend.scores[j])){
+                    continue;
+                }
+                currentFriendScore += friend.scores[j];
+            }
             $.ajax({
                 type: "GET",
                 url: "/api/friends"
-            }).then(function(results2){
-                console.log(results2);
+            }).then(function(results){
+                var mindifference = 100;
+                var friendmatch;
+                for (i=0; i<results.length; i++){
+                    if(results[i].name==friend.name){
+                        continue;
+                    }
+                    var scores = results[i].scores.split(",");
+                    var score = 0;
+                    for (j=0; j<scores.length; j++){
+                        if(scores[j] == "NaN"){
+                            continue;
+                        }
+                        score += parseInt(scores[j]);
+                    }
+                    var diff = Math.abs(score - currentFriendScore);
+                    if( diff < mindifference){
+                        mindifference = diff;
+                        friendmatch = results[i];
+                    }
+                }
+                if(friendmatch!=null){
+                    $("#container").hide();
+                    $("#result").html(
+                        "<p>"+friendmatch.name+"</p>"+
+                        "<p>score: "+mindifference+"</p>"+
+                        "<img src='"+friendmatch.picture_link+"'/>"
+                    );
+                }
             })
         })
-    })
-})
+    });
